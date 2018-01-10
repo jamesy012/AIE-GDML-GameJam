d700c6c96251b2548445cd567710f10dc4a1805e
James Owen <jamesowen012@gmail.com>
2018-01-10 16:35:43 +1100

    Player now only moves and can flip gravity when on the ground added checked to work out of the player is on the ground
    added new Layer called Player
    
    changed the Frictionless physics material to have no friction

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,17 +10,39 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour {
 
+    /// <summary>
+    /// reference to the rigidbody
+    /// </summary>
     private Rigidbody m_rigidbody;
 
+    /// <summary>
+    /// movement speed of the player
+    /// </summary>
     public float m_moveSpeed = 100;
+
+    /// <summary>
+    /// flag to check if the player is grounded
+    /// </summary>
+    public bool m_isGrounded = false;
+
+    private LayerMask m_playerLayerMask;
 
     // Use this for initialization
     void Start() {
         m_rigidbody = GetComponent<Rigidbody>();
+        m_playerLayerMask = LayerMask.NameToLayer("Player");
+
+        if(gameObject.layer != m_playerLayerMask) {
+            Debug.LogError("Please make the players layer to be 'Player'");
+        }
 
         Collider collider = GetComponent<Collider>();
-        if(collider.material == null || collider.material.name.Contains("Default")) {
-            Debug.LogError("The player colldier should have the FrictionLess Physics Material");
+        if (collider != null) {
+            if (collider.material == null || collider.material.name.Contains("Default")) {
+                Debug.LogError("The player colldier should have the FrictionLess Physics Material");
+            }
+        } else {
+            Debug.LogError("The player does not have a collider");
         }
 
         //set up the constraints
@@ -33,6 +55,22 @@ public class PlayerMovement : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        //calc grounded
+        //work out which way the gravity is going
+        float isGravityUp = Mathf.Sign(Physics.gravity.y);
+        //the character is offset from being centered, it's anchored to the bottom side of the player
+        Vector3 playerCharacterOffset = new Vector3(0, Mathf.Clamp01(Physics.gravity.y) * 2, 0);
+        //draw a debug ray to show off where the ray is
+        Debug.DrawRay(transform.position + playerCharacterOffset, Vector3.up * isGravityUp * 2);
+        //finally work out if the player is on the ground
+        m_isGrounded = Physics.Raycast(transform.position + playerCharacterOffset, Vector3.up * isGravityUp, 2, ~m_playerLayerMask.value);
+        //m_isGrounded = Physics.Raycast(transform.position + offset, Vector3.up * isGravityUp, out hit, 2, ~m_playerLayerMask.value);
+
+        //if the player is not grounded dont run the movement/gravity flip code
+        if (!m_isGrounded) {
+            return;
+        }
+
         //get key inputs
         bool keyLeft = Input.GetKey(KeyCode.A) | Input.GetKey(KeyCode.LeftArrow);
         bool keyRight = Input.GetKey(KeyCode.D) | Input.GetKey(KeyCode.RightArrow);
@@ -66,7 +104,7 @@ public class PlayerMovement : MonoBehaviour {
     private void FlipGravity() {
         Vector3 gravity = Physics.gravity;
         gravity.y *= -1;
-        Physics.gravity = gravity; 
+        Physics.gravity = gravity;
     }
 
 
