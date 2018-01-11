445e71ef2fea8ee5b1b11d65663611c6d5859053
Ben Guest <Reagle606@gmail.com>
2018-01-11 16:29:43 +1100

    stars

--- Assets/Scripts/EndOfLevel.cs
+++ Assets/Scripts/EndOfLevel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class EndOfLevel : MonoBehaviour
@@ -14,12 +15,22 @@ public class EndOfLevel : MonoBehaviour
     public int silverStar;
     public int bronzeStar;
     public Level currentLevel;
+    public Animator m_starAnim;
+    public Image m_levelStar;
+    public List<Sprite> m_starSprites;
+    public List<GameObject> m_stars;
 
     private void Start()
     {
         player = GameObject.Find("Player");
         playerCollider = player.GetComponent<Collider>();
+        m_stars.Add( GameObject.Find("BronzeStar"));
+        m_stars.Add(GameObject.Find("SilverStar"));
+        m_stars.Add(GameObject.Find("GoldStar"));
+        m_stars.Add(GameObject.Find("PlatStar"));
+      
 
+   
     }
 
     private void OnTriggerEnter(Collider other)
@@ -32,26 +43,50 @@ public class EndOfLevel : MonoBehaviour
                 {
                     GameObject.Find("Player").GetComponent<Player>().m_currentLevel.m_door.m_animtor.SetTrigger("Close");
                 }
-
-                if (currentLevel.m_movesDone <= currentLevel.platStar)
+                Debug.Log(currentLevel.m_movesDone);
+                if (currentLevel.m_movesDone == currentLevel.platStar)
                 {
-                    //give plat star
+                    Debug.Log("help");
+                    m_stars[3].SetActive(true);
+                    m_stars[2].SetActive(false);
+                    m_stars[1].SetActive(false);
+                    m_stars[0].SetActive(false);
+                    m_levelStar.sprite = m_starSprites[3];
+                    m_levelStar.color = Color.white;
+                    m_starAnim.SetTrigger("Go");
                 }
-
-                if (currentLevel.m_movesDone <= goldStar && currentLevel.m_movesDone > currentLevel.platStar)
+                else if (currentLevel.m_movesDone <= currentLevel.goldStar && currentLevel.m_movesDone > currentLevel.platStar)
                 {
-                    //give gold star
+                    Debug.Log("Gold");
+                    m_stars[3].SetActive(false);
+                    m_stars[2].SetActive(true);
+                    m_stars[1].SetActive(false);
+                    m_stars[0].SetActive(false);
+                    m_levelStar.sprite = m_starSprites[2];
+                    m_levelStar.color = Color.white;
+                    m_starAnim.SetTrigger("Go");
                 }
-
-                if (currentLevel.m_movesDone <= silverStar && currentLevel.m_movesDone > currentLevel.goldStar)
+                else if (currentLevel.m_movesDone <= currentLevel.silverStar && currentLevel.m_movesDone > currentLevel.goldStar)
                 {
-                    //give silver star
+                    Debug.Log("Silver");
+                    m_stars[3].SetActive(false);
+                    m_stars[2].SetActive(false);
+                    m_stars[1].SetActive(true);
+                    m_stars[0].SetActive(false);
+                    m_levelStar.sprite = m_starSprites[1];
+                    m_levelStar.color = Color.white;
+                    m_starAnim.SetTrigger("Go");
                 }
-                else
+                else if(currentLevel.m_movesDone <= currentLevel.bronzeStar && currentLevel.m_movesDone > currentLevel.silverStar)
                 {
-                    //give bronze star
+                    m_stars[3].SetActive(false);
+                    m_stars[2].SetActive(false);
+                    m_stars[1].SetActive(false);
+                    m_stars[0].SetActive(true);
+                    m_levelStar.sprite = m_starSprites[0];
+                    m_levelStar.color = Color.white;
+                    m_starAnim.SetTrigger("Go");
                 }
-
                 GameObject.Find("Player").GetComponent<Player>().m_currentLevel = newLevel;
                 newLevel.GetComponent<Level>().StartLevel();
                 m_active = false;
