71f45a01319a4a9067fcf9b1a46bc803489cf50f
Addison Stewart <addison.stewart@hotmail.com>
2018-01-11 13:47:12 +1100

    Added background music and implemented some changes to start the star reward system

--- Assets/Scripts/EndOfLevel.cs
+++ Assets/Scripts/EndOfLevel.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class EndOfLevel : MonoBehaviour
 {
+    GameObject player;
     Collider playerCollider;
     public bool m_active;
     public Level newLevel;
+    public bool changeBackgroundMusic;
+    public AudioClip newBackGroundMusic;
+    public int goldStar;
+    public int silverStar;
+    public int bronzeStar;
+    private Level currentLevel;
 
     private void Start()
     {
-        playerCollider = GameObject.Find("Player").GetComponent<Collider>();
+        player = GameObject.Find("Player");
+        playerCollider = player.GetComponent<Collider>();
+        currentLevel = player.GetComponent<Player>().m_currentLevel.GetComponent<Level>();
+
     }
 
     private void OnTriggerEnter(Collider other)
@@ -23,13 +33,23 @@ public class EndOfLevel : MonoBehaviour
                 {
                     GameObject.Find("Player").GetComponent<Player>().m_currentLevel.m_door.m_animtor.SetTrigger("Close");
                 }
+
+                //if (currentLevel.m_movesDone <= currentLevel)
+                //{
+                //    //give player gold star
+                //}
                 GameObject.Find("Player").GetComponent<Player>().m_currentLevel = newLevel;
                 newLevel.GetComponent<Level>().StartLevel();
                 m_active = false;
             }
+            if (changeBackgroundMusic)
+            {
+                SoundManager.StopBGM(false, 0);
+                SoundManager.PlayBGM(newBackGroundMusic, true, 10.0f);
+            }
 
+            
         }
-       
-        
+
     }
 }
